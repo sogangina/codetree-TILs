4ef30d9e372f3cc61b817b35c44e05f27e51b5fc
Gina Roe <122879737+sogangina@users.noreply.github.com>
2024-01-25 21:48:04 +0900

    Title: 2개씩 그룹짓기, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 240125/2개씩 그룹짓기/group-of-pairs.py
+++ 240125/2개씩 그룹짓기/group-of-pairs.py
@@ -1,6 +1,7 @@
 def function(n, array):
     array.sort()
-    return array[n] + array[n-1]
+    new = [array[i]+array[2*n-1-i] for i in range(0,n)]
+    return max(new)
 
 n = int(input())
 array = list(map(int, input().split()))
