386f8cfbc77d698e554ea541aaf481bac45a8758
Gina Roe <122879737+sogangina@users.noreply.github.com>
2024-01-25 09:44:03 +0900

    Title: 함수를 이용한 소수 판별, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240125/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240125/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -1,11 +1,5 @@
 def isprime(n):
-    result = True
-    root = int(n**0.5) + 1
-    for i in range(2, root):
-        if n % i == 0:
-            result = False
-            break
-    return result
+    return n in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
 a,b = map(int, input().split())
 
