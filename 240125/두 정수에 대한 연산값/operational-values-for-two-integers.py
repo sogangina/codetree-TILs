ef8fc4f39f632c0095fd94e4fa6e705b52d99414
Gina Roe <122879737+sogangina@users.noreply.github.com>
2024-01-25 11:46:28 +0900

    Title: 두 정수에 대한 연산값, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240125/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -0,0 +1,11 @@
+def function(a,b):
+    if a > b:
+        a += 25
+        b *= 2
+    else:
+        a *= 2
+        b += 25
+    return a,b
+
+a,b = map(int, input().split())
+print(*function(a,b))
