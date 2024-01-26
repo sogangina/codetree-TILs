c22343d63e694a2b8fcd79e16d40b3e7adaa3a95
Gina Roe <122879737+sogangina@users.noreply.github.com>
2024-01-26 16:36:41 +0900

    Title: 2진수로 변환하기, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240126/2진수로 변환하기/convert-to-binary.py
@@ -0,0 +1,10 @@
+def decode(n):
+    decimal = []
+    while n != 0:
+        decimal.append(n%2)
+        n //= 2
+    return decimal[::-1]
+
+n = int(input())
+for i in decode(n):
+    print(i, end='')
