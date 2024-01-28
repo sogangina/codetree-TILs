5ceaa891ed85af6d22b828012c382469dbd69a6d
Gina Roe <122879737+sogangina@users.noreply.github.com>
2024-01-28 10:45:36 +0900

    Title: 사각형의 총 넓이 2, Time: 125ms, Memory: 25MB, Status: Passed - CodeTree

--- 240128/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 240128/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -1,13 +1,21 @@
+OFFSET = 100
+MAX_R = 200
+
 n = int(input())
-grid = [[0 for _ in range(200)] for _ in range(200)]
-for _ in range(n):
-    x1, y1, x2, y2 = map(int, input().split())
-    for i in range(y1,y2):
-        for j in range(x1,x2):
-            grid[i+100][j+100] = 1
+rects = [tuple(map(int, input().split())) for _ in range(n)]
+grid = [[0 for _ in range(MAX_R+1)] for _ in range(MAX_R+1)]
+
+for x1, y1, x2, y2 in rects:
+    x1, y1 = x1+OFFSET, y1+OFFSET
+    x2, y2 = x2+OFFSET, y2+OFFSET
+
+    for y in range(y1,y2):
+        for x in range(x1,x2):
+            grid[y][x] = 1
+
 area = 0
-for row in grid:
-    for each in row:
-        if each:
-            area += 1
+for y in range(MAX_R+1):
+    for x in range(MAX_R+1):
+        if grid[y][x]: area += 1
+
 print(area)
